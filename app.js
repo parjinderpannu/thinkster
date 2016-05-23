ff5121b6018658339d74a0e7e5c48d0c09713a1d
Parjinder <ppannu@cisco.com>
2016-05-22 18:02:46 -0700

    Bind the .posts variable in our controller to the posts array in our service:

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ var app = angular.module('flapperNews', []);
     function($scope, posts){
       $scope.test = 'Hello World!';
   // Post Variables
-      $scope.posts = [
-  	{title: 'post 1',upvotes: 5},
-  	{title: 'post 2',upvotes: 2},
-  	{title: 'post 3',upvotes: 15},
-  	{title: 'post 4',upvotes: 9},
-  	{title: 'post 5',upvotes: 4},
-      ];
+      $scope.posts = posts.posts;
   // Adding a addPost function
       $scope.addPost = function(){
         if(!$scope.title || $scope.title === '') { return; }
