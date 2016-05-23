4ca4d1395f8bc22157d2de088325f414f35459d2
Parjinder <ppannu@cisco.com>
2016-05-22 18:01:29 -0700

    Now that we've created our service, lets go ahead and move our posts variable to it && add that service to your controller

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', ['$scope', 
-  function($scope){
-    $scope.test = 'Hello World!';
-// Post Variables
-    $scope.posts = [
-	{title: 'post 1',upvotes: 5},
-	{title: 'post 2',upvotes: 2},
-	{title: 'post 3',upvotes: 15},
-	{title: 'post 4',upvotes: 9},
-	{title: 'post 5',upvotes: 4},
-    ];
-// Adding a addPost function
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.title,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
-// Adding a Increase upvote count func
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
+  app.controller('MainCtrl', ['$scope','posts', 
+    function($scope, posts){
+      $scope.test = 'Hello World!';
+  // Post Variables
+      $scope.posts = [
+  	{title: 'post 1',upvotes: 5},
+  	{title: 'post 2',upvotes: 2},
+  	{title: 'post 3',upvotes: 15},
+  	{title: 'post 4',upvotes: 9},
+  	{title: 'post 5',upvotes: 4},
+      ];
+  // Adding a addPost function
+      $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.title,
+          upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+      };
+  // Adding a Increase upvote count func
+      $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+      };
+
+      app.factory('posts', [function(){
+        var o = {
+        posts: []
+        };
+        return o;
+      }]);
   } 
 ]);
