01b19d5e52f87be349fad7582a46dff8e9c2881a
Parjinder <ppannu@cisco.com>
2016-05-22 18:04:45 -0700

    To get started, lets include the library after we include Angular in our index.html && Because we are adding an external module, we need to be sure to include it as a dependency in our app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
   app.controller('MainCtrl', ['$scope','posts', 
     function($scope, posts){
